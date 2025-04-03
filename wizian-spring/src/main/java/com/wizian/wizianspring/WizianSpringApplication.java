dc2001368921c35fc3ea78f82a0a3bba755ddece
41817 <bangjungbin@gmail.com>
2025-04-03 17:13:46 +0900

    Initial commit

---
+++ wizian-spring/src/main/java/com/wizian/wizianspring/WizianSpringApplication.java
@@ -0,0 +1,13 @@
+package com.wizian.wizianspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WizianSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WizianSpringApplication.class, args);
+    }
+
+}
---
+++ wizian-spring/src/test/java/com/wizian/wizianspring/WizianSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wizian.wizianspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WizianSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
