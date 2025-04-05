8a2c2f4db42ef2724b870aa1add3806050d1ebd7
41817 <bangjungbin@gmail.com>
2025-04-05 09:40:10 +0900

    AllClass 페이지 구현

---
+++ wizian-next/src/app/(app)/course/courseStudent/page.js
@@ -0,0 +1,202 @@
+"use client"
+
+import React from "react";
+
+const AllClass = () => {
+    return (
+        <div className="main-content">
+            <div className="container-fluid">
+                <a href="/dashboard">메인 페이지 /</a>&ensp;<a href="#">모든 강의 정보</a>
+                <div className="row">
+                    <button type="button" className="btn btn-success col-lg-offset-10 margin-bottom-30">
+                        <i className="fa fa-refresh fa-spin"></i> 조회하기
+                    </button>
+                </div>
+
+                <div id="toastr-demo" className="panel col">
+                    <div className="panel-body row">
+                        <div className="col-md-3">
+                            <strong>캠퍼스 위치</strong>&emsp;&emsp;&emsp;
+                            <select className="navbar">
+                                <option value="cheese">Cheese</option>
+                                <option value="tomatoes">Tomatoes</option>
+                                <option value="mozarella">Mozzarella</option>
+                                <option value="mushrooms">Mushrooms</option>
+                                <option value="pepperoni">Pepperoni</option>
+                                <option value="onions">Onions</option>
+                            </select>
+                        </div>
+
+                        <div className="col-md-3">
+                            <strong>개설여부</strong>&emsp;&emsp;&emsp;
+                            <select className="navbar">
+                                <option value="cheese">Cheese</option>
+                                <option value="tomatoes">Tomatoes</option>
+                                <option value="mozarella">Mozzarella</option>
+                                <option value="mushrooms">Mushrooms</option>
+                                <option value="pepperoni">Pepperoni</option>
+                                <option value="onions">Onions</option>
+                            </select>
+                        </div>
+
+                        <div className="col-md-3">
+                            <strong>대표강사</strong>&emsp;&emsp;&emsp;
+                            <select className="navbar">
+                                <option value="cheese">Cheese</option>
+                                <option value="tomatoes">Tomatoes</option>
+                                <option value="mozarella">Mozzarella</option>
+                                <option value="mushrooms">Mushrooms</option>
+                                <option value="pepperoni">Pepperoni</option>
+                                <option value="onions">Onions</option>
+                            </select>
+                        </div>
+
+                        <div className="col-md-3">
+                            <strong><i className="lnr lnr-magnifier"></i> 검색</strong>
+                            <input type="text" className="form-control" placeholder="강의명 입력"/>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-md-8">
+                        <div className="panel panel-scrolling">
+                            <div className="panel-heading">
+                                <h3 className="panel-title">강의 리스트</h3>
+                            </div>
+                            <div className="panel-body">
+                                <table className="table table-striped">
+                                    <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>First Name</th>
+                                        <th>Last Name</th>
+                                        <th>Username</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Steve</td>
+                                        <td>Jobs</td>
+                                        <td>@steve</td>
+                                    </tr>
+                                    <tr>
+                                        <td>2</td>
+                                        <td>Simon</td>
+                                        <td>Philips</td>
+                                        <td>@simon</td>
+                                    </tr>
+                                    <tr>
+                                        <td>3</td>
+                                        <td>Jane</td>
+                                        <td>Doe</td>
+                                        <td>@jane</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-md-4">
+                        <div className="panel">
+                            <div className="panel-heading">
+                                <h3 className="panel-title">과정정보</h3>
+                            </div>
+                            <div className="panel-body">
+                                <p>번호</p>
+                                <input type="text" className="form-control" placeholder="text field"/>
+                                <br/>
+                                <p>강의명</p>
+                                <input type="password" className="form-control" value="asecret"/>
+                                <br/>
+                                <p>대표강사</p>
+                                <input type="password" className="form-control" value="asecret"/>
+                                <br/>
+                                <p>강의실</p>
+                                <input type="password" className="form-control" value="asecret"/>
+                                <br/>
+                                <p>수료일</p>
+                                <input type="password" className="form-control" value="asecret"/>
+                                <br/>
+                                <p>수강인원</p>
+                                <input type="password" className="form-control" value="asecret"/>
+                                <br/>
+                                <p>개설여부</p>
+                                <input type="password" className="form-control" value="asecret"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-md-6">
+                        <div className="panel">
+                            <div className="panel-heading">
+                                <h3 className="panel-title">강의 시간표</h3>
+                            </div>
+                            <div className="panel-body">
+                                <table className="table table-bordered">
+                                    <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>First Name</th>
+                                        <th>Last Name</th>
+                                        <th>Username</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Steve</td>
+                                        <td>Jobs</td>
+                                        <td>@steve</td>
+                                    </tr>
+                                    <tr>
+                                        <td>2</td>
+                                        <td>Simon</td>
+                                        <td>Philips</td>
+                                        <td>@simon</td>
+                                    </tr>
+                                    <tr>
+                                        <td>3</td>
+                                        <td>Jane</td>
+                                        <td>Doe</td>
+                                        <td>@jane</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-md-6">
+                        <div id="panel-scrolling-demo" className="panel">
+                            <div className="panel-heading">
+                                <h3 className="panel-title">강의계획서</h3>
+                            </div>
+                            <div className="panel-body">
+                                <p>Objectively network visionary methodologies via best-of-breed users.
+                                    Phosfluorescently initiate go forward leadership skills before an expanded array of
+                                    infomediaries. Monotonectally incubate web-enabled communities rather than
+                                    process-centric.</p>
+                                <p>Objectively network visionary methodologies via best-of-breed users.
+                                    Phosfluorescently initiate go forward leadership skills before an expanded array of
+                                    infomediaries. Monotonectally incubate web-enabled communities rather than
+                                    process-centric.</p>
+                                <p>Objectively network visionary methodologies via best-of-breed users.
+                                    Phosfluorescently initiate go forward leadership skills before an expanded array of
+                                    infomediaries. Monotonectally incubate web-enabled communities rather than
+                                    process-centric.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default AllClass;
