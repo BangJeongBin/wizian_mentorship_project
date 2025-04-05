f64d094acedcd96470748f476e933a1809cee296
41817 <bangjungbin@gmail.com>
2025-04-05 14:17:14 +0900

    CourseProblem 페이지 생성

---
+++ wizian-next/src/app/(app)/course/courseProblem/page.js
@@ -0,0 +1,248 @@
+"use client"
+
+import React from "react";
+
+const CourseProblem = () => {
+    return (
+        <div className="main-content">
+            <div className="container-fluid">
+                <a href="/dashboard">메인 페이지 /</a>&ensp;<a href="#">진행중인 강의 /</a>&ensp;<a href="#">과제관리</a>
+                <div className="row">
+                    <button type="button" className="btn btn-success col-lg-offset-10 margin-bottom-30">
+                        <i className="fa fa-refresh fa-spin"></i> 조회하기
+                    </button>
+                </div>
+
+                <div id="toastr-demo" className="panel col">
+                    <div className="panel-body row">
+                        <div className="col-md-4">
+                            <strong>출결현황</strong>&emsp;&emsp;&emsp;
+                            <select className="navbar">
+                                <option value="cheese">출석</option>
+                                <option value="tomatoes">결석</option>
+                                <option value="mozarella">지각</option>
+                                <option value="mushrooms">Mushrooms</option>
+                                <option value="pepperoni">Pepperoni</option>
+                                <option value="onions">Onions</option>
+                            </select>
+                        </div>
+
+                        <div className="col-md-4">
+                            <strong>출결일자</strong>&emsp;&emsp;&emsp;
+                            <select className="navbar">
+                                <option value="cheese">Cheese</option>
+                                <option value="tomatoes">Tomatoes</option>
+                                <option value="mozarella">Mozzarella</option>
+                                <option value="mushrooms">Mushrooms</option>
+                                <option value="pepperoni">Pepperoni</option>
+                                <option value="onions">Onions</option>
+                            </select>
+                        </div>
+
+                        <div className="col-md-2">
+                            <strong><i className="lnr lnr-magnifier"></i> 검색</strong>
+                            <input type="text" className="form-control" placeholder="학생명 입력"/>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-md-12">
+                        
+                        <div className="col-lg-offset-9">
+                            <button className="btn btn-primary" type="button">
+                                출석
+                            </button>
+                            &emsp;
+                            <button className="btn btn-warning" type="button">
+                                지각
+                            </button>
+                            &emsp;
+                            <button className="btn btn-danger" type="button">
+                                결석
+                            </button>
+                        </div>
+
+                        <div className="panel panel-scrolling">
+                            <div className="panel-heading">
+                                <h3 className="panel-title">학생 리스트</h3>
+                            </div>
+                            <div className="panel-body">
+                                <table className="table table-striped">
+                                    <thead>
+                                    <tr>
+                                        <th><input type="checkbox"/></th>
+                                        <th>#</th>
+                                        <th>First Name</th>
+                                        <th>Last Name</th>
+                                        <th>Username</th>
+                                        <th>Username</th>
+                                        <th>Username</th>
+                                        <th>Username</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <td><input type="checkbox"/></td>
+                                        <td>1</td>
+                                        <td>Steve</td>
+                                        <td>Jobs</td>
+                                        <td>@steve</td>
+                                        <td>@steve</td>
+                                        <td>@steve</td>
+                                        <td>
+                                            <span className="label label-info">출석</span>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td><input type="checkbox"/></td>
+                                        <td>2</td>
+                                        <td>Simon</td>
+                                        <td>Philips</td>
+                                        <td>@simon</td>
+                                        <td>@steve</td>
+                                        <td>@steve</td>
+                                        <td>
+                                            <span className="label label-warning">지각</span>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td><input type="checkbox"/></td>
+                                        <td>3</td>
+                                        <td>Jane</td>
+                                        <td>Doe</td>
+                                        <td>@jane</td>
+                                        <td>@steve</td>
+                                        <td>@steve</td>
+                                        <td>
+                                            <span className="label label-danger">결석</span>
+                                        </td>
+                                    </tr>
+
+                                    <tr>
+                                        <td><input type="checkbox"/></td>
+                                        <td>3</td>
+                                        <td>Jane</td>
+                                        <td>Doe</td>
+                                        <td>@jane</td>
+                                        <td>@steve</td>
+                                        <td>@steve</td>
+                                        <td>
+                                            <span className="label label-success">출결 전</span>
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-md-6">
+                        <div className="panel">
+                            <div className="panel-heading">
+                                <h3 className="panel-title">학생 정보</h3>
+                            </div>
+                            <div className="panel-body">
+                                <p>학생 번호</p>
+                                <input type="text" className="form-control" placeholder="text field"/>
+                                <br/>
+                                <p>이름</p>
+                                <input type="password" className="form-control" value="asecret"/>
+                                <br/>
+                                <p>이메일</p>
+                                <input type="password" className="form-control" value="asecret"/>
+                                <br/>
+                                <p>성별</p>
+                                <input type="password" className="form-control" value="asecret"/>
+                                <br/>
+                                <p>연락처</p>
+                                <input type="password" className="form-control" value="asecret"/>
+                                <br/>
+                                <p>주소</p>
+                                <input type="password" className="form-control" value="asecret"/>
+                                <br/>
+                                <p>회원가입일</p>
+                                <input type="password" className="form-control" value="asecret"/>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-md-6">
+                        <div className="panel panel-scrolling">
+                            <div className="panel-heading">
+                                <h3 className="panel-title">출결 이력</h3>
+                            </div>
+                            <div className="panel-body">
+                                <table className="table table-striped">
+                                    <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>First Name</th>
+                                        <th>Last Name</th>
+                                        <th>Username</th>
+                                        <th>Username</th>
+                                        <th>Username</th>
+                                        <th>Username</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Steve</td>
+                                        <td>Jobs</td>
+                                        <td>@steve</td>
+                                        <td>@steve</td>
+                                        <td>@steve</td>
+                                        <td>
+                                            <span className="label label-info">출석</span>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>2</td>
+                                        <td>Simon</td>
+                                        <td>Philips</td>
+                                        <td>@simon</td>
+                                        <td>@steve</td>
+                                        <td>@steve</td>
+                                        <td>
+                                            <span className="label label-warning">지각</span>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>3</td>
+                                        <td>Jane</td>
+                                        <td>Doe</td>
+                                        <td>@jane</td>
+                                        <td>@steve</td>
+                                        <td>@steve</td>
+                                        <td>
+                                            <span className="label label-danger">결석</span>
+                                        </td>
+                                    </tr>
+
+                                    <tr>
+                                        <td>3</td>
+                                        <td>Jane</td>
+                                        <td>Doe</td>
+                                        <td>@jane</td>
+                                        <td>@steve</td>
+                                        <td>@steve</td>
+                                        <td>
+                                            <span className="label label-success">출결 전</span>
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default CourseProblem;
