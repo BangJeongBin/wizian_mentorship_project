ae18f8cf06855453e4cb78c7e8c160dfe1382824
41817 <bangjungbin@gmail.com>
2025-04-07 17:57:47 +0900

    courseStudent sweetalert 적용

--- wizian-next/src/app/(app)/course/courseStudent/page.js
+++ wizian-next/src/app/(app)/course/courseStudent/page.js
@@ -1,74 +1,92 @@
 "use client"
 
 import React from "react";
+import Swal from "sweetalert2";
 
 const CourseStudent = () => {
+    // sweetAlert
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        Swal.fire({
+            title: '변경사항을 적용하시겠습니까?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: '예',
+            cancelButtonText: '아니오',
+            reverseButtons: true,
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire({
+                    title: '변경 완료되었습니다!',
+                    icon: 'success',
+                    confirmButtonText: '확인'
+                });
+            }
+        });
+    };
+
+
     return (
         <div className="main-content">
             <div className="container-fluid">
                 <a href="/dashboard">메인 페이지 /</a>&ensp;<a href="#">진행중인 강의 /</a>&ensp;<a href="#">수강학생</a>
-                <div className="row">
-                    <button type="button" className="btn btn-success col-lg-offset-10 margin-bottom-30">
-                        <i className="fa fa-refresh fa-spin"></i> 조회하기
-                    </button>
-                </div>
 
-                <div id="toastr-demo" className="panel col">
-                    <div className="panel-body row">
-                        <div className="col-md-3">
-                            <strong>강의 종류</strong>&emsp;&emsp;&emsp;
-                            <select className="navbar">
-                                <option value="cheese">Cheese(진행중)</option>
-                                <option value="tomatoes">Tomatoes</option>
-                                <option value="mozarella">Mozzarella</option>
-                                <option value="mushrooms">Mushrooms</option>
-                                <option value="pepperoni">Pepperoni</option>
-                                <option value="onions">Onions</option>
-                            </select>
-                        </div>
+                <form name="" id="" method="post" onSubmit={handleSubmit}>
+                    <div className="row">
+                        <button type="submit" className="btn btn-success col-lg-offset-10 margin-bottom-30">
+                            <i className="fa fa-refresh fa-spin"></i> 조회하기
+                        </button>
+                    </div>
 
-                        <div className="col-md-3">
-                            <strong>진행 상태</strong>&emsp;&emsp;&emsp;
-                            <select className="navbar">
-                                <option value="cheese">진행중인 학생</option>
-                                <option value="tomatoes">수료한 학생</option>
-                                <option value="mozarella">Mozzarella</option>
-                                <option value="mushrooms">Mushrooms</option>
-                                <option value="pepperoni">Pepperoni</option>
-                                <option value="onions">Onions</option>
-                            </select>
-                        </div>
+                    <div id="toastr-demo" className="panel col">
+                        <div className="panel-body row">
+                            <div className="col-md-3">
+                                <strong>강의 종류</strong>&emsp;&emsp;&emsp;
+                                <select className="navbar">
+                                    <option value="cheese">Cheese(진행중)</option>
+                                    <option value="tomatoes">Tomatoes</option>
+                                    <option value="mozarella">Mozzarella</option>
+                                    <option value="mushrooms">Mushrooms</option>
+                                    <option value="pepperoni">Pepperoni</option>
+                                    <option value="onions">Onions</option>
+                                </select>
+                            </div>
 
-                        <div className="col-md-3">
-                            <strong>성별</strong>&emsp;&emsp;&emsp;
-                            <select className="navbar">
-                                <option value="cheese">Cheese</option>
-                                <option value="tomatoes">Tomatoes</option>
-                                <option value="mozarella">Mozzarella</option>
-                                <option value="mushrooms">Mushrooms</option>
-                                <option value="pepperoni">Pepperoni</option>
-                                <option value="onions">Onions</option>
-                            </select>
-                        </div>
+                            <div className="col-md-3">
+                                <strong>진행 상태</strong>&emsp;&emsp;&emsp;
+                                <select className="navbar">
+                                    <option value="cheese">진행중인 학생</option>
+                                    <option value="tomatoes">수료한 학생</option>
+                                    <option value="mozarella">Mozzarella</option>
+                                    <option value="mushrooms">Mushrooms</option>
+                                    <option value="pepperoni">Pepperoni</option>
+                                    <option value="onions">Onions</option>
+                                </select>
+                            </div>
 
-                        <div className="col-md-3">
-                            <strong><i className="lnr lnr-magnifier"></i> 검색</strong>
-                            <input type="text" className="form-control" placeholder="학생명 입력"/>
+                            <div className="col-md-3">
+                                <strong>성별</strong>&emsp;&emsp;&emsp;
+                                <select className="navbar">
+                                    <option value="cheese">Cheese</option>
+                                    <option value="tomatoes">Tomatoes</option>
+                                    <option value="mozarella">Mozzarella</option>
+                                    <option value="mushrooms">Mushrooms</option>
+                                    <option value="pepperoni">Pepperoni</option>
+                                    <option value="onions">Onions</option>
+                                </select>
+                            </div>
+
+                            <div className="col-md-3">
+                                <strong><i className="lnr lnr-magnifier"></i> 검색</strong>
+                                <input type="text" className="form-control" placeholder="학생명 입력"/>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </form>
 
                 <div className="row">
                     <div className="col-md-12">
-                        <div className="col-lg-offset-10">
-                            <button className="btn btn-primary" type="button">
-                                수정
-                            </button>
-                            &emsp;
-                            <button className="btn btn-danger" type="button">
-                                삭제
-                            </button>
-                        </div>
                         <div className="panel panel-scrolling">
                             <div className="panel-heading">
                                 <h3 className="panel-title">학생 리스트</h3>
