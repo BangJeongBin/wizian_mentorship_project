0f95f868dcd218c8176d43430b9c510f9de2468a
41817 <bangjungbin@gmail.com>
2025-04-14 18:33:52 +0900

    fetch end point 설정

--- wizian-next/src/app/(app)/archive/myProblem/page.js
+++ wizian-next/src/app/(app)/archive/myProblem/page.js
@@ -1,30 +1,185 @@
 "use client"
 
-import React from "react";
+import React, {useRef, useState} from "react";
 import Swal from "sweetalert2";
+import {useParams} from "next/navigation";
 
 const MyProblem = () => {
-    // sweetAlert
-    const SearchSubmit = (e) => {
-        e.preventDefault();
+    const [yearOptions, setYearOptions] = useState([]);
+    const sortYearRef = useRef(null);
+    const sortHalfRef = useRef(null);
+    const findkeyRef = useRef(null);
 
-        Swal.fire({
-            title: '변경사항을 적용하시겠습니까?',
-            icon: 'warning',
+    // 과제정보 리스트
+    const [selectedCourNos, setSelectedCourNos] = useState([]); // 체크박스 상태 저장
+    const [checkedData, setCheckedData] = useState([]); // 체크박스 값 저장
+
+    // 제출과제 리스트
+    const [selectedCourNossub, setSelectedCourNossub] = useState([]); // 체크박스 상태 저장
+    const [checkedDatasub, setCheckedDatasub] = useState([]); // 체크박스 값 저장
+
+    const [courseData, setCourseData] = useState({});
+
+    // 초기 값 저장
+    const params = useParams();
+
+    // 페이지네이션 사용
+    const cpg = params.cpg || 1;
+    const [cpgs, setCpgs] = useState(cpg);
+
+    const sortYear = params.sortYear || "default";
+    const sortHalf = params.sortHalf || "default";
+    const findkey = params.findkey || "all";
+
+    const pglink = `http://localhost:8080/api/inst/archive/myProblem/list`
+
+
+    // useEffect(() => {
+    const fetchData = async (sortYear, sortHalf, findkey) => {
+        const fetchURL = `${pglink}/${sortYear}/${sortHalf}/${findkey}/${cpg}`;
+
+        const result = await Swal.fire({
+            title: '모든 과정 정보를 조회하시겠습니까?',
+            icon: 'question',
             showCancelButton: true,
             confirmButtonText: '예',
             cancelButtonText: '아니오',
             reverseButtons: true,
-        }).then((result) => {
-            if (result.isConfirmed) {
-                Swal.fire({
-                    title: '변경 완료되었습니다!',
+        });
+
+        if (result.isConfirmed) {
+            try {
+                const response = await fetch(fetchURL, {
+                    headers: { 'Accept': 'application/json' }
+                });
+                if (!response.ok) {
+                    throw new Error(`HTTP 오류! 상태 코드: ${response.status}`);
+                }
+
+                const data = await response.json();
+                console.log(data);
+                setCourseData(data);
+
+                await Swal.fire({
+                    title: '조회가 완료되었습니다!',
                     icon: 'success',
                     confirmButtonText: '확인'
                 });
+            } catch (err) {
+                console.error('오류발생!', err);
+                Swal.fire({
+                    title: '데이터 조회 중 오류가 발생했습니다!',
+                    icon: 'error',
+                    confirmButtonText: '확인'
+                });
             }
-        });
+        }
     };
+
+    // }, []);
+
+
+    // 조회하기 버튼 클릭 시 이벤트
+    const goListSearch = async (e) => {
+        const sortYear = sortYearRef.current.value;
+        const sortHalf = sortHalfRef.current.value;
+        const findkey = findkeyRef.current.value || "all";
+        console.log("ssssssssssssss", courseData.infoList)
+
+        // <select> 옵션 처음 1회 저장
+        if (true) {
+            const year = [...new Set(courseData.infoList?.map(item => item.assignInfoYear))];
+            setYearOptions(year);
+        }
+
+        fetchData(sortYear, sortHalf, findkey);
+    };
+
+    // 페이지네이션 다음 버튼
+    const nextListPage = async () => {
+        const nextPage = cpgs + 1;
+        setCpgs(nextPage);
+        await fetchDataPage(nextPage);
+    };
+
+    // 페이지네이션 이전 버튼
+    const preListPage = async () => {
+        const nextPage = cpgs - 1;
+        setCpgs(nextPage);
+        await fetchDataPage(nextPage);
+    };
+
+    // 페이지네이션 함수
+    const fetchDataPage = async (page) => {
+        const sortYear = sortYearRef.current.value;
+        const sortHalf = sortHalfRef.current.value;
+        const findkey = findkeyRef.current.value || "all";
+
+        const fetchURL = `http://localhost:8080/api/inst/archive/myProblem/list/${sortYear}/${sortHalf}/${findkey}/${page}`;
+
+        try {
+            const res = await fetch(fetchURL, {
+                headers: { 'Accept': 'application/json' }
+            });
+            const data = await res.json();
+            setCourseData(data);
+        } catch (err) {
+            console.error('오류 발생:', err);
+        }
+    };
+
+    // 과제정보 개별 체크박스 선택
+    const handleCheckbox = (assignInfoNo) => {
+        setCheckedData(assignInfoNo);
+        console.log(">>> 과제정보 개별 체크박스 ", checkedData);
+        if (selectedCourNos.includes(assignInfoNo)) {
+            setSelectedCourNos(selectedCourNos.filter(no => no !== assignInfoNo));
+        } else {
+            setSelectedCourNos([...selectedCourNos, assignInfoNo]);
+        }
+
+    };
+
+    // 과제정보 헤더 체크박스 선택
+    const handleAllCheckbox = (e) => {
+        if (e.target.checked) {
+            const allNos = courseData.infoList?.map(cls => cls.assignInfoNm);
+            setSelectedCourNos(allNos);
+        } else {
+            setSelectedCourNos([]);
+        }
+    };
+
+    const isAllChecked = courseData?.infoList?.length > 0 &&
+        selectedCourNos.length === courseData.infoList.length;
+
+
+    // 제출과제 개별 체크박스 선택
+    const handleCheckboxsub = (assignInfoNosub) => {
+        setCheckedDatasub(assignInfoNosub);
+        console.log(">>> 제출과제 개별 체크박스 ", checkedDatasub);
+        if (selectedCourNossub.includes(assignInfoNosub)) {
+            setSelectedCourNossub(selectedCourNossub.filter(no => no !== assignInfoNosub));
+        } else {
+            setSelectedCourNossub([...selectedCourNossub, assignInfoNosub]);
+        }
+
+    };
+
+    // 제출과제 헤더 체크박스 선택
+    const handleAllCheckboxsub = (e) => {
+        if (e.target.checked) {
+            const allNos = courseData.submitList?.map(cls => cls.studnt.stdntNo);
+            setSelectedCourNossub(allNos);
+        } else {
+            setSelectedCourNossub([]);
+        }
+    };
+
+    const isAllCheckedsub = courseData?.submitList?.length > 0 &&
+        selectedCourNossub.length === courseData.submitList.length;
+
+
 
     // sweetAlert
     const ProblemModify = (e) => {
